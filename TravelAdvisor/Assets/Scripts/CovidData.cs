494e8beac7ad9d974db8e4179289d039bb39cd8a
Ali <alips96@gmail.com>
2020-12-05 14:47:08 +0100

    Data Download

---
+++ TravelAdvisor/Assets/Scripts/CovidData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CovidData
+{
+    public string Province_State { get; set; }
+    public string Country_Region { get; set; }
+    public int Confirmed { get; set; }
+    public int Deaths { get; set; }
+    public int Recovered { get; set; }
+    public int Active { get; set; }
+    public string Combined_Key { get; set; }
+    public double Incident_Rate { get; set; }
+    public double Case_Fatality_Ratio { get; set; }
+
+    internal static CovidData ParseRow(string row)
+    {
+        string[] column = TrimRow(ref row);
+
+        return new CovidData()
+        {
+            Province_State = column[2],
+            Country_Region = column[3],
+            Confirmed = Convert.ToInt32(column[7]),
+            Deaths = Convert.ToInt32(column[8]),
+            Recovered = Convert.ToInt32(column[9]),
+            Active = Convert.ToInt32(column[10]),
+            Combined_Key = column[11],
+            Incident_Rate = Convert.ToDouble(column[12]),
+            Case_Fatality_Ratio = Convert.ToDouble(column[13])
+        };
+    }
+
+    private static string[] TrimRow(ref string row)
+    {
+        row = row.Trim(new char[] { '"' });
+        string[] column = row.Split(',');
+        return column;
+    }
+
+}
---
+++ TravelAdvisor/Assets/Scripts/DataCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// So far, we can download and save the file in the destination folder.
+/// Download process occurs only once per day.
+/// </summary>
+public class DataCollection : MonoBehaviour
+{
+    string yesterday;
+
+    void Start()
+    {
+        yesterday = GetYesterday();
+        bool shouldDownloadCorpus = CheckIfDownloadNecessary(yesterday);
+
+        if (shouldDownloadCorpus)
+        {
+            PlayerPrefs.DeleteAll();
+            StartCoroutine(DownloadCorpus());
+            PlayerPrefs.SetInt(yesterday, 1);
+        }
+    }
+
+    private IEnumerator DownloadCorpus()
+    {
+        string url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + yesterday + ".csv";
+
+        UnityWebRequest webRequest = new UnityWebRequest(url, UnityWebRequest.kHttpVerbGET);
+        string path = Path.Combine(Application.dataPath + "/Data", "covidData.csv");
+        webRequest.downloadHandler = new DownloadHandlerFile(path);
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.isNetworkError || webRequest.isHttpError)
+            Debug.LogError(webRequest.error);
+    }
+
+    private bool CheckIfDownloadNecessary(string yesterday)
+    {
+        return PlayerPrefs.GetInt(yesterday) != 1;
+    }
+
+    private string GetYesterday()
+    {
+        DateTime yesterday = DateTime.Today.AddDays(-1); //because we need to download the data of the previous day.
+
+        int day = yesterday.Day;
+        int month = yesterday.Month;
+        string dayCorrectFormat = day < 10 ? "0" + day.ToString() : day.ToString();
+        string monthCorrectFormat = month < 10 ? "0" + month.ToString() : month.ToString();
+
+        return monthCorrectFormat + "-" + dayCorrectFormat + "-" + yesterday.Year.ToString();
+    }
+}
