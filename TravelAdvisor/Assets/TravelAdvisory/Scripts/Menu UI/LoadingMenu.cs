12e8d2dc53577914a17886f361b92536fee94b77
Ali <alips96@gmail.com>
2020-12-17 23:17:21 +0100

    Loading Menu

---
+++ TravelAdvisor/Assets/TravelAdvisory/Scripts/Menu UI/LoadingMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingMenu : MonoBehaviour
+{
+    private RectTransform rectComponent;
+    private Image imageComp;
+
+    [SerializeField] private float speed = 0.7f;
+
+
+    void Start()
+    {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        imageComp.fillAmount = 0.0f;
+    }
+
+    void Update()
+    {
+        if (imageComp.fillAmount != 1f)
+        {
+            imageComp.fillAmount = imageComp.fillAmount + Time.deltaTime * speed;
+        }
+        else
+        {
+            imageComp.fillAmount = 0.0f;
+        }
+    }
+}
