c3e87eb811221654ca6276558df02a07bd88b979
Ali <alips96@gmail.com>
2020-12-23 14:52:21 +0100

    Results UI

--- TravelAdvisor/Assets/TravelAdvisory/Scripts/Menu UI/Result.cs
+++ TravelAdvisor/Assets/TravelAdvisory/Scripts/Menu UI/Result.cs
@@ -32,8 +32,7 @@ public class Result : MonoBehaviour
     [SerializeField] private TMP_Text DStatus;
 
     [SerializeField] private TMP_Text OverallStatus;
-
-    Dictionary<string, Condition> StateConditionsDic;
+    private Condition[] StateConditionsArr;
 
     private void Start()
     {
@@ -44,29 +43,39 @@ public class Result : MonoBehaviour
 
     private void AssignColorsToStats()
     {
-        foreach (KeyValuePair<string, Condition> item in StateConditionsDic)
+        for (byte i = 0; i < StateConditionsArr.Length; i++)
         {
-            AnalyzeAndAssignAColor(item);
+            AnalyzeData(i);
         }
+
+        SetColors();
     }
 
-    private void AnalyzeAndAssignAColor(KeyValuePair<string, Condition> item)
+    private void SetColors()
     {
-        switch (item.Key)
+        SIncidentRate.color = StateConditionsArr[0].Color;
+        SCaseFatalityRatio.color = StateConditionsArr[1].Color;
+        DIncidentRate.color = StateConditionsArr[2].Color;
+        DCaseFatalityRatio.color = StateConditionsArr[3].Color;
+    }
+
+    private void AnalyzeData(byte index)
+    {
+        switch (index)
         {
-            case "SIR":
+            case 0: //StartingPoint Incident Rate
                 AnalyzeIncidentRate(false);
                 break;
 
-            case "SCF":
+            case 1: //StartingPoint Case Fatality Ratio
                 AnalyzeCaseFatalityRatio(false);
                 break;
 
-            case "DIR":
+            case 2: //Destination Incident Rate
                 AnalyzeIncidentRate(true);
                 break;
 
-            case "DCF":
+            case 3: //Destination Case Fatality Ratio
                 AnalyzeCaseFatalityRatio(true);
                 break;
         }
@@ -74,12 +83,103 @@ public class Result : MonoBehaviour
 
     private void AnalyzeCaseFatalityRatio(bool isDestination)
     {
-        throw new NotImplementedException();
+        if (isDestination)
+        {
+            double destinationCFRatio = destination.Case_Fatality_Ratio;
+
+            if (destinationCFRatio < 0.5f)
+            {
+                StateConditionsArr[3] = new Condition(Color.green, 2);
+            }
+            else
+            if (destinationCFRatio >= 0.5f && destinationCFRatio < 1.5f)
+            {
+                StateConditionsArr[3] = new Condition(Color.yellow, 3);
+            }
+            else
+            if (destinationCFRatio >= 1.5f && destinationCFRatio < 3)
+            {
+                StateConditionsArr[3] = new Condition(new Color(1.0f, 0.64f, 0.0f), 4); //Orange
+            }
+            else // >= 3
+            {
+                StateConditionsArr[3] = new Condition(Color.red, 5);
+            }
+        }
+        else
+        {
+            double StartingPointCFRatio = startingPoint.Case_Fatality_Ratio;
+
+            if(StartingPointCFRatio < 0.5f)
+            {
+                StateConditionsArr[1] = new Condition(Color.green, 2);
+            }
+            else
+            if (StartingPointCFRatio >= 0.5f && StartingPointCFRatio < 1.5f)
+            {
+                StateConditionsArr[1] = new Condition(Color.yellow, 3);
+            }
+            else
+            if (StartingPointCFRatio >= 1.5f && StartingPointCFRatio < 3)
+            {
+                StateConditionsArr[1] = new Condition(new Color(1.0f, 0.64f, 0.0f), 4); //Orange
+            }
+            else // >= 3
+            {
+                StateConditionsArr[1] = new Condition(Color.red, 5);
+            }
+        }
     }
 
     private void AnalyzeIncidentRate(bool isDestination)
     {
-        throw new NotImplementedException();
+        if (isDestination) //destination
+        {
+            double destinationIncidentRate = destination.Incident_Rate;
+
+            if(destinationIncidentRate < 500)
+            {
+                StateConditionsArr[2] = new Condition(Color.green, 2);
+            }
+            else
+            if(destinationIncidentRate >= 500 && destinationIncidentRate < 1500)
+            {
+
+                StateConditionsArr[2] = new Condition(Color.yellow, 3);
+            }
+            else
+            if(destinationIncidentRate >= 1500 && destinationIncidentRate < 3000)
+            {
+                StateConditionsArr[2] = new Condition(new Color(1.0f, 0.64f, 0.0f), 4); //Orange
+            }
+            else // >= 3000
+            {
+                StateConditionsArr[2] = new Condition(Color.red, 5);
+            }
+        }
+        else //starting point
+        {
+            double StartingPointIncidentRate = startingPoint.Incident_Rate;
+
+            if (StartingPointIncidentRate < 500)
+            {
+                StateConditionsArr[0] = new Condition(Color.green, 2);
+            }
+            else
+            if (StartingPointIncidentRate >= 500 && StartingPointIncidentRate < 1500)
+            {
+                StateConditionsArr[0] = new Condition(Color.yellow, 3);
+            }
+            else
+            if (StartingPointIncidentRate >= 1500 && StartingPointIncidentRate < 3000)
+            {
+                StateConditionsArr[0] = new Condition(new Color(1.0f, 0.64f, 0.0f), 4); //Orange
+            }
+            else // >= 3000
+            {
+                StateConditionsArr[0] = new Condition(Color.red, 5);
+            }
+        }
     }
 
     private void SetStatsUI()
@@ -110,20 +210,21 @@ public class Result : MonoBehaviour
         startingPoint = Resources.Load<Region>("Start");
         destination = Resources.Load<Region>("End");
 
-        StateConditionsDic = new Dictionary<string, Condition>()
-        {
-            {"SIR", Condition.Green },
-            {"SCF", Condition.Green },
-            {"DIR", Condition.Green },
-            {"DCF", Condition.Green }
-        };
+        StateConditionsArr = new Condition[4];
     }
 
-    private enum Condition : byte
+    private class Condition
     {
-        Green = 2,
-        Yellow = 3,
-        Orange = 4,
-        Red = 5
+        public Condition(Color _color, byte _code)
+        {
+            color = _color;
+            code = _code;
+        }
+
+        private readonly Color color;
+        private readonly byte code;
+
+        public Color Color => color;
+        public byte Code => code;
     }
 }
