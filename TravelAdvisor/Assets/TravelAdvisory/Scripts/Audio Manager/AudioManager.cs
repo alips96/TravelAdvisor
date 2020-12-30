2c6b7215744542c489ccd6a61290d3439dde5d75
Ali <alips96@gmail.com>
2020-12-30 22:14:48 +0100

    Audio Manager

---
+++ TravelAdvisor/Assets/TravelAdvisory/Scripts/Audio Manager/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource mainTheme;
+    private Slider slider;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void SetVolume() //Called by options menu slider
+    {
+        mainTheme.volume = slider.value;
+    }
+}
